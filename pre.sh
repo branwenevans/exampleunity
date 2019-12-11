5370185bfb85390fca0a0b02863818b9009b6677
Branwen Evans <evans.branwen@gmail.com>
2019-12-11 10:12:38 -0600

    Update pre.sh

--- pre.sh
+++ pre.sh
@@ -1,8 +1,2 @@
 export UNITY_DOTNET_TEST=$(dirname $UNITY_EXE)/../NetCore/Sdk/dotnet
-echo $UNITY_DOTNET_TEST
-$UNITY_DOTNET_TEST
-echo 'test'
-echo $PROJECTDIR
-echo 'test'
-printenv
-echo 'test'
+msbuild
