d473eee23b40e08959d310d6ff087354db32ccd3
Branwen Evans <evans.branwen@unity3d.com>
2020-05-15 09:57:48 -0500

    pre

--- pre.sh
+++ pre.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-cat ~/.upmconfig.toml
-echo "Hello World" >> ~/.upmconfig.toml
-cat ~/.upmconfig.toml
+#cat ~/.upmconfig.toml
+#echo "Hello World" >> ~/.upmconfig.toml
+#cat ~/.upmconfig.toml
+
+echo "[npmAuth.'https://nexus3.domain.com/repository/repo/']" >> ~/.upmconfig.toml
+echo "token = '$TEST'" >> ~/.upmconfig.toml
+echo "email = 'email@domain.com'" >> ~/.upmconfig.toml
+echo "alwaysAuth = true" >> ~/.upmconfig.toml
